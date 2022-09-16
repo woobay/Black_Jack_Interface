aff5e557477e1940cecfe87785aa7af522cf9367
Francis Bolduc <f.bolduc@outlook.com>
2022-09-16 19:55:30 -0400

    Tp Pas mal Fini

--- src/main/java/com/example/tp3/BlackjackApp.java
+++ src/main/java/com/example/tp3/BlackjackApp.java
@@ -4,12 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import static javafx.application.Application.launch;
@@ -17,204 +16,242 @@ import static javafx.application.Application.launch;
 public class BlackjackApp extends Application {
 
     private static BlackjackGame game;
+    TextField moneyField = new TextField();
+    TextField betField = new TextField();
+    ListView<String> dealerCardsField = new ListView<>();
+    TextField dealerPointsField = new TextField();
+    ListView<String>  playerCardsField = new ListView<>();
+    TextField winnerField = new TextField();
+    TextField playerPointField = new TextField();
+    Label errorMessageLabel = new Label();
+    Button playButton = new Button();
+    Button exitButton = new Button();
+    Button hitButton = new Button();
+    Button standButton = new Button();
 
 
     public static void main(String[] args) {
-        game = new BlackjackGame();
         launch();
     }
+    public void start(Stage stage){
+        game = new BlackjackGame();
+        moneyField.setText(Double.toString(game.loadMoney()));
+        disableButton(hitButton, true);
+        disableButton(standButton, true);
 
-	// affiche le message Out of money! Would you like to add more? (y/n):. Si le joueur tappe y alors la fonction reset la balance du joueur au 100 et retourne true. False Sinon.
-    private static boolean buyMoreChips() {
-            return Console.getString("Out of money! Would you like to add more? (y/n): ").equalsIgnoreCase("y");
-    }
-    
-	// affiche le message Bet amount, lire la valeur de la mise saisi par le joueur. Valide cette valeur. Si la valeur n'est pas valide afficher le message Bet must be between
-    private static void getBetAmount() {
-        game.setBet(Console.getDouble("Enter bet amount: ", game.getMinBet(), game.getMaxBet()));
-     }
-
-	// Affiche le message Hit or Stand? (h/s): et puis retourne ce que le joueur a tappe.
-    private static String getHitOrStand() {
-        String myArray[] = {"h", "s"};
-        return Console.getString("Hit or Stand? (h/s): ", myArray);
-    }
-
-	// affiche les cartes dans la main du courtier et les cartes dans la main du joueur
-	    private static void showHands() {
-            showPlayerHand();
-            showDealerHand();
-        }
-
-	// affiche le message DEALER'S SHOW CARD et puis affiche le deuxieme carte dans la main du courtier
-    private static void showDealerShowCard() {
-        System.out.println(game.getDealerShowCard().display());
-        System.out.println();
-    }
-
-	// affiche le message DEALER'S CARDS et puis affiche les cartes dans la main du courtier
-    private static void showDealerHand() {
-        System.out.println("Dealer's cards: ");
-        Card[] cards = game.getDealerHand().getCards().toArray(new Card[0]);
-        for (Card card: cards) {
-            System.out.println(card.display());
-        }
-    }
-
-	// affiche le message YOUR CARDS et puis affiche les cartes dans la main du joueur
-    private static void showPlayerHand() {
-        System.out.println("Your cards: ");
-        Card[] cards = game.getPlayerHand().getCards().toArray(new Card[0]);
-        for (Card card: cards) {
-            System.out.println(card.display());
-        }
-    }
-
-	// affiche Total money:  et le montant total
-    private static void showMoney() {
-        System.out.printf("Total money: %s",game.getTotalMoney());
-        System.out.println();
-
-    }
-    private static void showWinner() {
-        System.out.println();
-        showPlayerHand();
-        System.out.printf("YOUR POINTS: %d%n", game.getPlayerHand().getPoints());
-        System.out.println();
-
-        showDealerHand();
-        System.out.printf("DEALER'S POINTS: %d%n%n", game.getDealerHand().getPoints());
-
-        if(game.isPush()) {
-            System.out.println("Push!");
-        } else if(game.getPlayerHand().isBlackjack()) {
-            System.out.println("BLACKJACK! You win!");
-            game.addBlackjackToTotal();
-        } else if (game.playerWins()) {
-            System.out.println("You win!");
-            game.addBetToTotal();
-        } else {
-            System.out.println("Sorry, you lose.");
-            game.subtractBetFromTotal();
-        }
-        showMoney();
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.TOP_CENTER);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
+        grid.setPadding(new Insets(50, 50, 50, 50));
         grid.setVgap(10);
-        grid.setHgap(25);
+        grid.setHgap(10);
+
+        grid.add(errorMessageLabel,0, 11);
+        errorMessageLabel.setTextFill(Color.RED);
 
         Label moneyLabel = new Label("Money: ");
         grid.add(moneyLabel, 0, 0);
 
-        TextField moneyStart = new TextField();
-        grid.add(moneyStart, 1, 0);
-        moneyStart.setText(Double.toString(game.loadMoney()));
+        grid.add(moneyField, 1, 0);
+        moneyField.setEditable(false);
 
         Label betLabel = new Label("Bet: ");
         grid.add(betLabel, 0,1 );
 
-
-        TextField betField = new TextField();
         grid.add(betField, 1, 1);
-
 
         Label dealerLabel = new Label("DEALER");
         grid.add(dealerLabel, 0, 2);
 
         Label dealerCardsLabel = new Label("Cards: ");
         grid.add(dealerCardsLabel, 0,3);
-
-        TextArea dealCardsField = new TextArea();
-        grid.add(dealCardsField, 1,3);
+        grid.add(dealerCardsField, 1,3);
 
         Label dealerPointsLabel = new Label("Points: ");
         grid.add(dealerPointsLabel, 0, 4);
 
-        TextField dealerPointsField = new TextField();
         grid.add(dealerPointsField, 1, 4);
+        dealerPointsField.setEditable(false);
 
         Label playerLabel = new Label("PLAYER");
         grid.add(playerLabel, 0, 5);
 
         Label playerCardsLabel = new Label("Cards: ");
         grid.add(playerCardsLabel, 0,6);
-
-        TextArea playerCardsField = new TextArea();
         grid.add(playerCardsField, 1,6);
 
-    //  Cartes PLayer
         Label playerPointLabel = new Label("Points: ");
         grid.add(playerPointLabel, 0, 7);
-
-        TextField playerPointField = new TextField();
         grid.add(playerPointField, 1, 7);
+        playerPointField.setEditable(false);
 
-    //  Bouton Hit N Stand
+        for(int i = 0; i < grid.getColumnCount(); i++){
+            ColumnConstraints col = new ColumnConstraints(210);
+            grid.getColumnConstraints().add(col);
+        }
 
-        Button hitButton = new Button();
+        // Bouton Play et Exit
+        playButton.setText("Play");
+        exitButton.setText("Exit");
+
+        HBox playButtonBox = new HBox(10);
+        playButtonBox.getChildren().addAll(playButton, exitButton);
+        playButtonBox.setAlignment(Pos.BOTTOM_LEFT);
+        grid.add(playButtonBox, 0, 10);
+
+
+        playButton.setOnAction(event -> {
+
+            if(playButton.getText().equals("Play Again")){
+                errorMessage("");
+                betField.setText("");
+                winnerField.setText("");
+                playerCardsField.getItems().clear();
+                dealerCardsField.getItems().clear();
+                dealerPointsField.setText("");
+                playerPointField.setText("");
+                moneyField.setText(showMoney());
+                playButton.setText("Play");
+            }
+            else{
+                try {
+                    if(game.isValidBet(Double.parseDouble(betField.getText()))){
+                        errorMessage("");
+                        disableButton(hitButton, false);
+                        disableButton(standButton, false);
+                        disableButton(playButton, true);
+
+                        game.deal();
+                        showDealerShowCard();
+                        showPlayerHand();
+
+                        playerPointField.setText(String.valueOf(game.getPlayerHand().getPoints()));
+                    }else{
+                        errorMessage("Bet not valide!!");
+                        return;
+                    }
+                    if (game.getPlayerHand().isBlackjack() || game.getDealerHand().isBlackjack()){
+                        showWinner();
+                    }
+                }catch (NumberFormatException e){
+                    errorMessage("Bet not valide!!");
+                }
+            }
+
+        });
+        exitButton.setOnAction(actionEvent -> {
+            System.exit(0);
+        });
+
+        //  Button Hit and Stand
+
         hitButton.setText("Hit");
-
-        Button standButton = new Button();
         standButton.setText("Stand");
-
-
         HBox HitStandButtonBox = new HBox(10);
-        HitStandButtonBox.getChildren().add(hitButton);
-        HitStandButtonBox.getChildren().add(standButton);
-
+        HitStandButtonBox.getChildren().addAll(hitButton, standButton);
         HitStandButtonBox.setAlignment(Pos.BOTTOM_LEFT);
-
         grid.add(HitStandButtonBox, 0, 8);
 
-//  Resultat win or loose!
+        hitButton.setOnAction(event -> {
+            playerCardsField.getItems().clear();
+            game.hit();
+            showPlayerHand();
+            playerPointField.setText(String.valueOf(game.getPlayerHand().getPoints()));
+
+            if (game.getPlayerHand().isBust() || game.getPlayerHand().getPoints() == 21) {
+                dealerTurn();
+            }
+        });
+        standButton.setOnAction(event -> {
+            disableButton(hitButton, true);
+            disableButton(standButton, true);
+            disableButton(playButton, false);
+
+            dealerTurn();
+        });
+
+
+        //  Resultat win or loose!
 
         Label winnerLabel = new Label("RESULT: ");
         grid.add(winnerLabel, 0, 9);
 
-        TextField winnerField = new TextField();
         grid.add(winnerField, 1, 9);
+        playerPointField.setEditable(false);
 
-//    Bouton Play ou Exit
-
-        Button playButton = new Button();
-        playButton.setText("Play");
-
-        Button exitButton = new Button();
-        exitButton.setText("Exit");
-
-
-        HBox playButtonBox = new HBox(10);
-        playButtonBox.getChildren().add(playButton);
-        playButtonBox.getChildren().add(exitButton);
-
-        playButtonBox.setAlignment(Pos.BOTTOM_LEFT);
-
-        grid.add(playButtonBox, 0, 10);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        Scene scene = new Scene(grid, 320, 240);
+        Scene scene = new Scene(grid, 700, 700);
         stage.setTitle("BlackJack App!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void dealerTurn(){
+        dealerCardsField.getItems().clear();
+        game.stand();
+        showWinner();
+    }
+
+	// affiche le message DEALER'S SHOW CARD et puis affiche le deuxieme carte dans la main du courtier
+    private void showDealerShowCard() {
+        dealerCardsField.getItems().add(game.getDealerShowCard().display());
+    }
+
+	// affiche le message DEALER'S CARDS et puis affiche les cartes dans la main du courtier
+    private void showDealerHand() {
+        dealerCardsField.getItems().clear();
+        Card[] cards = game.getDealerHand().getCards().toArray(new Card[0]);
+        for (Card card: cards) {
+            dealerCardsField.getItems().add(card.display());
+        }
+    }
+
+	// affiche le message YOUR CARDS et puis affiche les cartes dans la main du joueur
+    private void showPlayerHand() {
+        Card[] cards = game.getPlayerHand().getCards().toArray(new Card[0]);
+        for (Card card: cards) {
+            playerCardsField.getItems().add(card.display());
+        }
+    }
+
+	// affiche Total money:  et le montant total
+    private String showMoney() {
+        return String.valueOf(game.getTotalMoney());
+    }
+    private void showWinner() {
+
+        showDealerHand();
+
+        disableButton(hitButton, true);
+        disableButton(standButton, true);
+        disableButton(playButton, false);
+        dealerPointsField.setText(String.valueOf(game.getDealerHand().getPoints()));
+
+        if(game.isPush()) {
+            winnerField.setText("Push!");
+        } else if(game.getPlayerHand().isBlackjack()) {
+            winnerField.setText("BLACKJACK! You win!");
+            game.addBlackjackToTotal();
+        } else if (game.playerWins()) {
+            winnerField.setText("You win!");
+            game.addBetToTotal();
+        } else {
+            if(game.getDealerHand().isBlackjack()) winnerField.setText("Dealer BLACKJACK!! Sorry, you lose");
+            else winnerField.setText("Sorry, you lose.");
+            game.subtractBetFromTotal();
+        }
+        moneyField.setText(showMoney());
+        if(game.isOutOfMoney()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("You are out of money");
+            alert.setContentText("If do you want play against, press Play Again button");
+            alert.showAndWait();
+            game.resetMoney();
+        }
+        playButton.setText("Play Again");
+    }
+    private void disableButton(Button button, boolean bol){
+        button.setDisable(bol);
+    }
+    private void errorMessage(String mess){
+        errorMessageLabel.setText(mess);
     }
 }
